57c49817d78b738ff5e3c1cca5852da73b8cc022
algoroot <algoroot524@gmail.com>
2023-02-11 16:24:53 +0900

    mock함수를 이용한 test

--- src/mock/check/test/check.test.js
+++ src/mock/check/test/check.test.js
@@ -1,1 +1,36 @@
-const check = require('../check');
+const check = require("../check");
+
+/**
+ *  mock 함수를 이용하면 실제로 함수를 구현하지 않아도 되며 좀 더 간편하게 확인할 수 있다.
+ *  https://jestjs.io/docs/mock-functions
+ */
+describe("check", () => {
+  let onSuccess;
+  let onFail;
+
+  beforeEach(() => {
+    onSuccess = jest.fn();
+    onFail = jest.fn();
+  });
+
+  it("should call onSuccess when predicate is true", () => {
+    check(() => true, onSuccess, onFail);
+
+    // onSuccess()가  한 번 호출된다.
+    // expect(onSuccess.mock.calls.length).toBe(1);
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    // expect(onSuccess.mock.calls[0][0]).toBe("yes");
+    expect(onSuccess).toHaveBeenCalledWith("yes");
+    // onFail()가  0 번 호출된다.
+    // expect(onFail.mock.calls.length).toBe(0);
+    expect(onFail).toHaveBeenCalledTimes(0);
+  });
+
+  it("should call onFail when predicate is false", () => {
+    check(() => false, onSuccess, onFail);
+
+    expect(onFail).toHaveBeenCalledTimes(1);
+    expect(onFail).toHaveBeenCalledWith("no");
+    expect(onSuccess).toHaveBeenCalledTimes(0);
+  });
+});
