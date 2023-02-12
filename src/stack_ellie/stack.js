51886764f4b6451074430ccb1d315ab8e96a5b9d
algoroot <algoroot524@gmail.com>
2023-02-12 16:14:53 +0900

    TDD를 이용한 Stack 구현 리팩토링

--- src/stack_ellie/stack.js
+++ src/stack_ellie/stack.js
@@ -1,31 +1,35 @@
-class Node {
-  constructor(item) {
-    this.item = item;
-    this.next = null;
-  }
-}
-
+/**
+ 
+ * 테스트 코드는 내부 구현사항을 테스트하는 것이 아닌, 사용자 입장에서 제공되어지는 인터페이스를 토대로 해서 테스트하는 것이 유지보수성이 높은 코드이다.
+ * 내부 구현사항을 리팩토링 할 때도 테스트 코드가 있기 때문에 안전하게 수정할 수 console.error('있다',있다)
+*/
 class Stack {
   constructor() {
-    this.array = [];
+    this._size = 0;
+    this.head = null;
   }
 
   size() {
-    return this.array.length;
+    return this._size;
   }
 
   push(item) {
-    return this.array.push(item);
+    const node = { item, next: this.head };
+    this.head = node;
+    this._size++;
   }
 
   pop() {
-    if (this.array.length === 0) throw new Error("Stack is empty.");
-    return this.array.pop();
+    if (this.head === null) throw new Error("Stack is empty.");
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.item;
   }
 
   peek() {
-    if (this.array.length === 0) throw new Error("Stack is empty.");
-    return this.array[this.size() - 1];
+    if (this.head === null) throw new Error("Stack is empty.");
+    return this.head.item;
   }
 }
 
